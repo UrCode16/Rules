35d36eaa535074bd43e483ac120c3a553327a77f
SuperBaronDEV <58933628+SuperBaronDEV@users.noreply.github.com>
2020-01-05 15:36:45 +0100

    Update and rename SRC/ SuperBaronDEV/ MyPlugin/Main.php to SRC/ SuperBaronDEV/Pravidla/Main.php

--- SRC/ SuperBaronDEV/Pravidla/Main.php
+++ SRC/ SuperBaronDEV/Pravidla/Main.php
@@ -1,7 +1,7 @@
 <?php
-namespace SuperBaronDEV\myplugin; 
+namespace SuperBaronDEV\pravidla; 
 use pocketmine\plugin\PluginBase; 
-class MyPlugin expands PluginBase{
+class Pravidla expands PluginBase{
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "pravidla"){
