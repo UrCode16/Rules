6866f06060607e77326f55eb55d0bad2744aa5f5
SuperBaronDEV <58933628+SuperBaronDEV@users.noreply.github.com>
2020-01-11 20:02:29 +0100

    Update Main.php

--- SRC/SuperBaronDEV/Rules/Main.php
+++ SRC/SuperBaronDEV/Rules/Main.php
@@ -1,11 +1,13 @@
 <?php
 namespace SuperBaronDEV\rules; 
 use pocketmine\plugin\PluginBase; 
+use pocketmine\command\Command;
+use pocketmine\command\ComamndSender;
 class Rules expands PluginBase{
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "rules"){
-      $sender->sendMessage("§ePravidla jsou-");
+      $sender->sendMessage("§eRules is§6-§71.don't spam    2.don't grief    §73.don't write bad words    4.don't ask for op");
       return false;
     }
     return false;
